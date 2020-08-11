6a9304684c91f328c89044e8aa3a1bd9a4eb23a8
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-08-11 18:00:22 +0200

    Add new projects images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -26,28 +26,38 @@ const Card = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  width: 350px;
-  height: 420px;
+  flex: 1 1 20vw;
+  height: 550px;
+  max-width: 380px;
+  padding: 30px;
   background: #e4b363;
   padding: 25px;
   margin: 25px;
   border-radius: 4px;
+  border: 1px #928a97;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
 
-  .img {
+  img {
     width: 100%;
+    max-width: 500px;
+    object-fit: cover;
+    object-position: center;
+    cursor: pointer;
+    max-height: 460px;
+    padding: 20px;
   }
 
   .name {
     font-size: 18px;
     font-weight: bold;
-    margin: 10px;
+    padding: 20px;
     text-align: center;
   }
 
   .description {
     font-size: 16px;
-    margin: 10px;
+    padding: 10px;
     text-align: center;
   }
 `
@@ -73,6 +83,7 @@ function Projects() {
           rel="noopener noreferrer"
         >
           <Card>
+            <img alt="survivor project" src="/survivor.png" />
             <div className="name">Survivor</div>
             <div className="description">
               An application built in React and Express that allows users to
@@ -80,7 +91,7 @@ function Projects() {
               has a search field for users to get information about other
               plants.
             </div>
-            <div className="technologies">
+            <div className="description">
               Technologies: Javascript, React, AWS, Node.js, Express.js,
               PostgreSQL
             </div>
@@ -94,13 +105,14 @@ function Projects() {
           rel="noopener noreferrer"
         >
           <Card>
+            <img alt="retrato project" src="/retrato.png" />
             <div className="name">Retrato</div>
             <div className="description">
               Single-page website inspired by Instagram with the theme of
               culture and big cities. The application permits any user to post a
               picture with a title and description, and comment on images.{' '}
             </div>
-            <div className="technologies">
+            <div className="description">
               Technologies: Javascript, Handlebars, Node.js, Express.js,
               PostgreSQL, Vue.js, AWS S3
             </div>
@@ -114,13 +126,17 @@ function Projects() {
           rel="noopener noreferrer"
         >
           <Card>
+            <img
+              alt="pop for popcorn lovers project"
+              src="/popforpopcornlovers.png"
+            />
             <div className="name">POP for Popcorn Lovers</div>
             <div className="description">
               Social network made in React for people who love popcorn. The
               single-page website enable users to add a profile, picture, see
               online friends, chat, make posts and manage friend requests.
             </div>
-            <div className="technologies">
+            <div className="description">
               Technologies: Javascript, React, Redux, Express.js, AWS, Node.js,
               PostgreSQL, Socket.IO
             </div>
@@ -134,13 +150,14 @@ function Projects() {
           rel="noopener noreferrer"
         >
           <Card>
+            <img alt="futurama project" src="/futurama.png" />
             <div className="name">Futurama Quotes</div>
             <div className="description">
               Single-page application created in React for the Futurama fans.
               The application allow users to search for a favorite character and
               see their famous quotes.
             </div>
-            <div className="technologies">
+            <div className="description">
               Technologies: Javascript, React, Redux
             </div>
           </Card>
