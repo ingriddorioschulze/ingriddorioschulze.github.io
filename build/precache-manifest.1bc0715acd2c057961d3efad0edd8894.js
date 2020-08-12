bdbf445e6753d8935594ef7b8f70ac5ff0f721fb
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-08-12 14:42:09 +0200

    Update package.json

---
+++ build/precache-manifest.1bc0715acd2c057961d3efad0edd8894.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996e98e5ead2221b53e534bfcf18b0d3",
+    "url": "/ingriddorioschulze.github.io/index.html"
+  },
+  {
+    "revision": "bf7ccf612624aecd68e9",
+    "url": "/ingriddorioschulze.github.io/static/js/2.d877e9bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ingriddorioschulze.github.io/static/js/2.d877e9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca4cb01af984baa2066",
+    "url": "/ingriddorioschulze.github.io/static/js/main.3afc4cbd.chunk.js"
+  },
+  {
+    "revision": "82e9407fbaf53d0d8c46",
+    "url": "/ingriddorioschulze.github.io/static/js/runtime-main.53c6a09b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.184a2c02f65580f6bf68909823796999.js"
+  "/ingriddorioschulze.github.io/precache-manifest.1bc0715acd2c057961d3efad0edd8894.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ingriddorioschulze.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
